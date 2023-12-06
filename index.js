e187155aca6eccf3ed1f52f750fc83db52d4b79e
Lee Bowyer <vanillacx@outlook.com>
2023-12-06 15:52:19 +0100

    Fixed Export Issue

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const {Schema, DataTypes} = require("./modules/Schema");
+const {Schema, SchemaError, DataTypes} = require("./modules/Schema");
 
 module.exports = {Schema, SchemaError, ...DataTypes, DataTypes}
